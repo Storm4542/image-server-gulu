5e98713b91dcb82a30af6221eabbddc590e321b6
Storm4542 <417002107@qq.com>
2018-12-19 16:35:42 +0800

    port

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ app.get('/preview/:key', cors(), function (req, res, next) {
         }
     })
 })
+var port = process.env.PORT || 3000
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(port)
