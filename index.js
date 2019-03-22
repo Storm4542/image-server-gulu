9ae10a502d5874096903119e38ca8108ae0ea879
Storm4542 <417002107@qq.com>
2019-03-22 11:28:56 +0800

    xx

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const multer = require('multer')
+var bodyParser = require('body-parser');
 const cors = require('cors')
 const upload = multer({
     dest: 'uploads/'
 })
-const p = require('path')
+const p = require('path');
 
 const app = express()
-
+app.use(bodyParser.json({
+    limit: '50mb',
+    extended: true
+})); // for parsing application/json
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 app.options('/upload', cors())
-app.get('/', function (req, res, next) {
-    res.send('图片上传接口')
-})
-app.post('/upload', cors(), upload.single('file'), function (req, res, next) {
-    console.log(req.file);
-    res.json({
-        key: req.file.filename
-    })
+
+app.post('/upload', cors(), function (req, res, next) {
+    console.log(req.body);
+    // res.json({
+    //     key: req.file.filename
+    // })
+    res.send(req.body)
 })
 app.get('/upload/:key', cors(), function (req, res, next) {
     res.sendFile(`uploads/${req.params.key}`, {
